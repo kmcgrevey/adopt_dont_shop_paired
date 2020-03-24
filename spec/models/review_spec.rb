0dc30b223750e856dcb14d01cb646f3dee44bf9f
Alex Lynch <mlynch5187@gmail.com>
2020-03-24 16:50:41 -0600

    Add validations for review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Review, type: :model do
   describe "relationships" do
     it {should belong_to :shelter}
   end
+
+  describe "validations" do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :rating}
+    it {should validate_presence_of :content}
+  end
 end
