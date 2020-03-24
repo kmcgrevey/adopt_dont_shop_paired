fc8b45a321697612b285c60174da563c43d7bff8
Alex Lynch <mlynch5187@gmail.com>
2020-03-24 14:59:30 -0600

    Update show shelter test to test for reviews

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -30,5 +30,25 @@ RSpec.describe "test shelters show page", type: :feature do
 
     expect(page).to have_content("Adoptable Pets")
     expect(page).to have_content("Create Pet")
+
+    it "see a list of reviews for shelter" do
+
+    lakeside_shelter = Shelter.create(name: "Lakeside Shelter",
+                                      address: "2914 Freetown Road",
+                                      city: "Columbia",
+                                      state: "Maryland",
+                                      zip: "21044")
+
+    review_1 = Shelter.reviews.create(title: "Best Shelter",
+                                      rating: 5,
+                                      content: "These guys really know how to run a shelter!",
+                                      picture: "https://www.rd.com/wp-content/uploads/2019/10/puppies-1-760x506.jpg")
+
+    visit "/shelters/#{lakeside_shelter.id}"
+
+    expect(page).to have_content(review_1.title)
+    expect(page).to have_content(review_1.rating)
+    expect(page).to have_content(review_1.content)
+    expect(page).to have_css("img[src*='#{review_1.picture}']")
   end
 end
