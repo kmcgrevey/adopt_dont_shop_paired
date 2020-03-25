b4af8986a43da4c74a54be2f8e3cb127311b9fec
Alex Lynch <mlynch5187@gmail.com>
2020-03-25 16:17:12 -0600

    Bolster shelter delete test to test that a shelter can be deleted with dependent reviews and dependent pets

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -38,4 +38,38 @@ RSpec.describe "test delete shelters", type: :feature do
     expect(page).to have_content(lakeside_shelter.name)
     expect(page).to_not have_content(parkside_shelter.name)
   end
+
+  it "can delete a shelter that has reviews and pets" do
+
+    parkside_shelter = Shelter.create(name: "Parkside Shelter",
+                                      address: "1234 Market Street",
+                                      city: "Denver",
+                                      state: "Colorado",
+                                      zip: "80230")
+
+    caesar = Pet.create(image: "https://thehappypuppysite.com/wp-content/uploads/2017/10/Cute-Dog-Names-HP-long.jpg",
+                        name: "Caesar",
+                        approx_age: "4",
+                        city: "Denver",
+                        sex: "Male",
+                        shelter: parkside_shelter)
+
+    review_1 = parkside_shelter.reviews.create!(title: "Best Shelter",
+                                    rating: 5,
+                                    content: "These guys really know how to run a shelter!",
+                                    picture: "https://www.rd.com/wp-content/uploads/2019/10/puppies-1-760x506.jpg")
+
+    visit "/shelters"
+
+    expect(page).to have_content(parkside_shelter.name)
+
+    click_link "Delete Shelter"
+
+    expect(page).to_not have_content(parkside_shelter.name)
+    expect(page).to_not have_content(review_1.title)
+
+    visit "/pets"
+
+    expect(page).to_not have_content(caesar.name)
+  end
 end
