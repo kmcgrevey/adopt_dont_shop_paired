740b6b2a25916c15dce737e16a7a2048dee90ee1
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-26 12:49:13 -0600

    Add test for favorite pet button and count

--- spec/features/favorites/add_favorite_spec.rb
+++ spec/features/favorites/add_favorite_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "As visitor" do
   describe "I see a favorite indicator" do
-    it "that shows a pet count on any page" do
+    it "that shows a favorite pet count on any page" do
       shelter_1 = Shelter.create!(name: "Burt's Barn",
                            address: "123 Sesame Street",
                            city: "New York",
@@ -28,6 +28,27 @@ RSpec.describe "As visitor" do
       visit "/pets/#{pet_1.id}"
       
       expect(page).to have_content("Favorites: 0")
+    end 
+    
+    it "click button to favorite a pet see a flash and favorite count increases by one" do
+      shelter_1 = Shelter.create!(name: "Burt's Barn",
+                           address: "123 Sesame Street",
+                           city: "New York",
+                           state: "NY",
+                           zip: "12345")
+      pet_1 = shelter_1.pets.create!(image: "https://assets.change.org/photos/3/yk/di/kLYkdIaPKknZpoD-800x450-noPad.jpg?1519383791",
+                             name: "Penelope",
+                             description: "A face only everyone could love!",
+                             approx_age: 1,
+                             sex: "female")
+      visit "/pets/#{pet_1.id}"
+
+      expect(page).to have_content("Favorites: 0")
+      
+      click_button "Favorite"
+      
+      expect(page).to have_content("Favorites: 1")
+      expect(page).to have_content("Penelope has been added to your favorites")
     end 
   end
 end
