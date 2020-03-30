92f5881c720196f3c0f7c123b7495b1df88cfc6d
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-29 23:13:49 -0600

    Add adoptable default status to pet table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200328205127) do
+ActiveRecord::Schema.define(version: 20200330051103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20200328205127) do
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
     t.string "description"
-    t.string "status"
+    t.string "status", default: "Adoptable"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
