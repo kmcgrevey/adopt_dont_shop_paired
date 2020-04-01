5a70edcbf74b25dc9638b8ba1af5b3b52e3708bf
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-31 18:46:19 -0600

    Add default picture to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200330051103) do
+ActiveRecord::Schema.define(version: 20200401003901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20200330051103) do
     t.string "title"
     t.integer "rating"
     t.string "content"
-    t.string "picture"
+    t.string "picture", default: "https://www.bigi.id/media/catalog/product/placeholder/default/default-no-image_2.png"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
