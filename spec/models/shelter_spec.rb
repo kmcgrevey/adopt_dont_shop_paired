7b5eaa5aa41ecab180b33edd57e4f056b7ab1738
Kevin McGrevey <niv3kmcg@gmail.com>
2020-12-02 10:28:55 -0700

    Updates Shelter spec

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -88,25 +88,13 @@ RSpec.describe Shelter, type: :model do
       expect(burts_barn.number_of_apps).to eq(1)
     end
 
-    xit '.has_apps' do
-      shelter_1 = Shelter.create!(name: "Burt's Barn",
-                              address: "123 Sesame Street",
-                              city: "New York",
-                              state: "NY",
-                              zip: "12345")
-
-      pet_1 = shelter_1.pets.create!(image: "https://assets.change.org/photos/3/yk/di/kLYkdIaPKknZpoD-800x450-noPad.jpg?1519383791",
+    it '.has_apps' do
+      pet_pend = @paulas_shelter.pets.create!(image: "https://assets.change.org/photos/3/yk/di/kLYkdIaPKknZpoD-800x450-noPad.jpg?1519383791",
                             name: "Penelope",
                             description: "A face only everyone could love!",
                             approx_age: 1,
                             sex: "female",
                             status: "Pending")
-
-      pet_2 = shelter_1.pets.create!(image: "https://thehappypuppysite.com/wp-content/uploads/2017/10/Cute-Dog-Names-HP-long.jpg",
-                          name: "Caesar",
-                          approx_age: "4",
-                          description: "Denver",
-                          sex: "Male")
 
       application_1 = Application.create!(name: "John Hutchinson",
                                   address: "4089 S. Broadway Street",
@@ -116,9 +104,9 @@ RSpec.describe Shelter, type: :model do
                                   phone: "215-367-8891",
                                   description: "I am loving and will provide a good home")
 
-      application_1.pets << [pet_1, pet_2]
+      application_1.pets << [@pet_1, pet_pend]
 
-      expect(shelter_1.has_apps).to eq(true)
+      expect(@paulas_shelter.has_apps).to eq(true)
     end
   end
 end
